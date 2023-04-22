ef5d6338eca66f928fe7e23a496db6c17a6fd313
EegleZ <maksstyzej@gmail.com>
2023-04-22 23:16:49 +0200

    Logika gry skończona

--- src/GUI/WskaznikWyniku.java
+++ src/GUI/WskaznikWyniku.java
@@ -4,19 +4,19 @@ import javax.swing.*;
 import java.awt.*;
 
 public class WskaznikWyniku extends JPanel {
-    JTextField gracz1 = new JTextField();
-    JTextField gracz2 = new JTextField();
+    JTextField gracz1 = new JTextField(12);
+    JTextField gracz2 = new JTextField(12);
     WskaznikWyniku() {
         this.setLayout(new BorderLayout());
-        this.setSize(100, 50);
+        this.setSize(200, 50);
         gracz1.setText("GRACZ 1: 0");
         gracz2.setText("GRACZ 2: 0");
         gracz1.setEditable(false);
         gracz2.setEditable(false);
         gracz1.setBounds(0, 0, 60, 30);
         gracz2.setBounds(60, 0, 60, 30);
-        this.add(gracz1, BorderLayout.EAST);
-        this.add(gracz2, BorderLayout.WEST);
+        this.add(gracz1, BorderLayout.WEST);
+        this.add(gracz2, BorderLayout.EAST);
     }
 
     public void ustawWynik(int gracz, int wynik) {
--- src/Logic/Gra.java
+++ src/Logic/Gra.java
@@ -7,6 +7,7 @@ public class Gra {
     public Gra(int pRozmiar) {
         ROZMIAR = pRozmiar;
         plansza = new Plansza(ROZMIAR);
+        tura= Plansza.Wlasciciel.GRACZ1;
     }
     public Plansza pobierzPlansze() {
         return plansza;
@@ -21,9 +22,9 @@ public class Gra {
             tura = Plansza.Wlasciciel.GRACZ1;
     }
     public int liczPunkty(Plansza.Wlasciciel gracz) {
-        //Rows
         int punkty = 0;
         boolean ciag = false;
+        //Poziomo
         for(int i=0; i<ROZMIAR; i++) {
             for(int j=0; j<ROZMIAR-1; j++) {
                 if(plansza.pobierzWlasiciela(i, j)==gracz &&
@@ -39,6 +40,99 @@ public class Gra {
                 }
             }
             ciag=false;
+        }
+        //Pionowo
+        for(int i=0; i<ROZMIAR; i++) {
+            for(int j=0; j<ROZMIAR-1; j++) {
+                if(plansza.pobierzWlasiciela(j, i)==gracz &&
+                        plansza.pobierzWlasiciela(j+1, i)==gracz) {
+                    ciag = true;
+                    punkty++;
+                    if(j==5)
+                        punkty++;
+                }
+                else if(ciag) {
+                    ciag=false;
+                    punkty++;
+                }
+            }
+            ciag=false;
+        }
+        //Skos / do połowy
+        for(int i=1; i<ROZMIAR; i++) {
+            for(int j=0; j<i; j++) {
+                if(plansza.pobierzWlasiciela(i-j, j)==gracz &&
+                plansza.pobierzWlasiciela(i-j-1, j+1)==gracz) {
+                    ciag=true;
+                    punkty++;
+//                    if(i==j+1)
+//                        punkty++;
+                }
+                else if(ciag) {
+                    ciag=false;
+                    punkty++;
+                }
+            }
+            //jedno dodatkowe wywolanie - jesli skonczy sie skos
+            if(ciag) {
+                ciag=false;
+                punkty++;
+            }
+        }
+        //Skos / od połowy do końca
+        //Szuka od końca tablicy do środka bez środkowej przekątnej (jest ona liczona na górze)
+        for(int j=ROZMIAR-2; j>0; j--){
+           for(int i=ROZMIAR-1; i>j; i--) {
+               if(plansza.pobierzWlasiciela(i, j+(ROZMIAR-1-i))==gracz &&
+                       plansza.pobierzWlasiciela(i-1, j+1+(ROZMIAR-1-i))==gracz) {
+                   ciag=true;
+                   punkty++;
+               }
+               else if(ciag) {
+                   ciag=false;
+                   punkty++;
+               }
+           }
+           //jedno dodatkowe wywolanie - jesli skonczy sie skos
+            if(ciag) {
+                ciag=false;
+                punkty++;
+            }
+        }
+        //Skos \ od 6,0 do połowy
+        for(int i=ROZMIAR-2; i>0; i--) {
+            for(int j=0; j<ROZMIAR-1-i ; j++) {
+                if(plansza.pobierzWlasiciela(i+j, j)==gracz &&
+                        plansza.pobierzWlasiciela(i+j+1, j+1)==gracz) {
+                    ciag=true;
+                    punkty++;
+                }
+                else if(ciag) {
+                    ciag=false;
+                    punkty++;
+                }
+            }
+            if(ciag) {
+                ciag=false;
+                punkty++;
+            }
+        }
+        for(int j=0; j<ROZMIAR-1; j++) {
+            for(int i=0; i<ROZMIAR-1-j; i++) {
+                if(plansza.pobierzWlasiciela(i, j+i)==gracz &&
+                        plansza.pobierzWlasiciela(i+1, j+i+1)==gracz) {
+                    ciag=true;
+                    punkty++;
+                }
+                else if(ciag) {
+                    ciag=false;
+                    punkty++;
+                }
+            }
+            if(ciag) {
+                ciag=false;
+                punkty++;
+            }
         }
         return punkty;
     }
